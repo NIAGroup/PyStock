ca67a9cbe68c2277ba934125a0ae29e972db0e9d
princton1 <princtonb@gmail.com>
2018-10-15 09:54:11 -0700

    comments added for clear description

--- TEST_PRINCTON/NIA_stocks_automation.py
+++ TEST_PRINCTON/NIA_stocks_automation.py
@@ -1,5 +1,11 @@
-import requests, alpha_vantage
+################################################################################
+####remember to include the "s" in the url so the proxy works effectively.
+####REMEMBER TO INSTALL the alpha_vantage library for this code.
+####alpha_vantage is a library we've found that's free and collects stock data.
+################################################################################
 
+import requests, alpha_vantage
+ 
 API_URL = "https://www.alphavantage.co/query"
 
 data = {
@@ -10,13 +16,11 @@ data = {
 	"interval": "weekly",
 	"apikey": "ksjendcksnfk",
 }
-http_proxy  = "http://10.10.1.10:3128"
-https_proxy = "https://10.10.1.11:1080"
-ftp_proxy   = "ftp://10.10.1.10:3128"
+
 proxies = {
 	"https" : "http://proxy-us.intel.com:911"
 }
 
 response = requests.get(API_URL, params=data,proxies=proxies) 
 for r in response:
-	print(r)
+	print(str(r).replace("\n","")+'\n')
