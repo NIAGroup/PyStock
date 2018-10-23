3b8f3aa4a241048805c9d9a6d01ca08242a38008
Princton Brennan <princtonbrennan@Princtons-MBP.attlocal.net>
2018-10-22 23:40:20 -0700

    test

--- TEST_PRINCTON/NIA_stocks_automation.py
+++ TEST_PRINCTON/NIA_stocks_automation.py
@@ -4,23 +4,45 @@
 ####alpha_vantage is a library we've found that's free and collects stock data.
 ################################################################################
 
-import json, requests, alpha_vantage, openpyxl
+import json, requests, alpha_vantage, openpyxl, argparse
 import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib import dates
 from openpyxl import Workbook
+
+
+def getArgs():
+    parser = argparse.ArgumentParser(description='stuff')
+    parser.add_argument('-s')
+    args = parser.parse_args()
+    s = args.s
+    return s
+
+def getLeTicks(array_corrected_x):
+	leTicks = []
+	for t in range(0,len(array_corrected_x)):
+		if t%4 == 0:
+			leTicks.append(array_corrected_x[t])
+
+	return leTicks
+
+stock_symbol = getArgs()
 row = 1
 col = 1
 wb = Workbook()
 ws = wb.create_sheet("Main")
-data_array = []
-array_corrected = []
+share_array = []
+year_array = []
+array_corrected_y = []
+array_corrected_x = []
 API_URL = "https://www.alphavantage.co/query"
 
 data = {
 	"function": "SMA",
 	"time_period": 10,
 	"series_type": "open",
-	"symbol": "MSFT",
-	"interval": "weekly",
+	"symbol": stock_symbol,
+	"interval": "monthly",
 	"apikey": "ksjendcksnfk",
 }
 
@@ -33,12 +55,6 @@ except Exception:
     response = requests.get(API_URL, params=data)
 arr = response.text.split("{")
 
-'''for l in arr:
-    detes =  l.replace("{","")
-    detes = detes.replace("\n","")
-    print(detes)
-    ws.cell(row,col,detes)
-    row+=1'''
 js = json.loads(response.text)
 d = js["Technical Analysis: SMA"]
 row = 1
@@ -48,13 +64,32 @@ for s in d:
 	if(row>1):
 	    stringer_a = str(s)
 	    stringer_b = str(d[s]["SMA"])
-	    data_array.append(float(stringer_b))
+	    year_array.append(str(stringer_a))
+	    share_array.append(float(stringer_b))
 	    print(stringer_a+"\t"+stringer_b)
 	    ws.cell(row-1,col,stringer_a)
 	    ws.cell(row-1,col+1,stringer_b)
 	row+=1
 wb.save("results.xlsx")
-for x in range(len(data_array)-1,-1,-1):
-	array_corrected.append(data_array[x])
-plt.plot(array_corrected)
+year_array =  matplotlib.dates.datestr2num(year_array)
+formatter = dates.DateFormatter('%Y-%m-%d')
+for x in range(len(year_array)-1,-1,-1):
+	array_corrected_x.append(year_array[x])
+for x in range(len(share_array)-1,-1,-1):
+	array_corrected_y.append(share_array[x])
+
+array_corrected_x = getLeTicks(array_corrected_x)
+array_corrected_y = getLeTicks(array_corrected_y)
+plt.figure().patch.set_facecolor('xkcd:mint green')
+plt.plot(array_corrected_x,array_corrected_y, color='darkorange',marker='o')
+plt.xlabel("Dates")
+plt.ylabel("Share Value")
+plt.title("Share Value Trends for: "+stock_symbol)
+plt.grid()
+the_ticks = array_corrected_x
+plt.xticks(the_ticks)
+plt.xticks(rotation=70)
+ax = plt.gcf().axes[0] 
+ax.xaxis.set_major_formatter(formatter)
+
 plt.show()
