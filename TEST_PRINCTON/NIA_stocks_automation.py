778fa5cbfdc78a73f0ce0a5beeeb29060cebb623
Princton Brennan <princtonbrennan@Princtons-MBP.attlocal.net>
2018-11-09 21:56:44 -0800

    comparison code added

--- TEST_PRINCTON/NIA_stocks_automation.py
+++ TEST_PRINCTON/NIA_stocks_automation.py
@@ -9,13 +9,23 @@ import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib import dates
 from openpyxl import Workbook
+from openpyxl.styles import Font, Alignment
 
 
 def getArgs():
     parser = argparse.ArgumentParser(description='stuff')
     parser.add_argument('-s')
+    #args = parser.parse_args()
+    parser.add_argument('-c')
     args = parser.parse_args()
-    s = args.s
+    if args.s == None and args.c == None:
+    	s = "INTC:MSFT"
+    elif args.s == None and args.c != None:
+    	s = "INTC:"+args.c
+    elif args.s != None and args.c == None:
+    	s = args.s+":INTC"
+    elif args.s != None and args.c != None:
+    	s = args.s+":"+args.c
     return s
 
 def getLeTicks(array_corrected_x):
@@ -26,22 +36,55 @@ def getLeTicks(array_corrected_x):
 
 	return leTicks
 
-stock_symbol = getArgs()
+stock_symbols = getArgs().split(":")
+stock_symbol_A = stock_symbols[0]
+stock_symbol_B = stock_symbols[1]
+
+print("stock A: "+stock_symbol_A)
+print("stock B: "+stock_symbol_B)
+
 row = 1
 col = 1
 wb = Workbook()
 ws = wb.create_sheet("Main")
-share_array = []
-year_array = []
-array_corrected_y = []
-array_corrected_x = []
+col_width = 25
+ws.column_dimensions['A'].width = col_width
+ws.column_dimensions['B'].width = col_width
+ws.column_dimensions['C'].width = col_width
+ws.column_dimensions['D'].width = col_width
+ws.cell(row,col,"Company A - Date").font = Font(bold=True, size=14)
+ws.cell(row,col+1,"Company A - Share Value").font = Font(bold=True, size=14)
+ws.cell(row,col+2,"Company B - Date").font = Font(bold=True, size=14)
+ws.cell(row,col+3,"Company B - Share Value").font = Font(bold=True, size=14)
+ws.cell(row,col).alignment = Alignment(vertical="center", horizontal="center", wrap_text="True")
+ws.cell(row,col+1).alignment = Alignment(vertical="center", horizontal="center", wrap_text="True")
+ws.cell(row,col+2).alignment = Alignment(vertical="center", horizontal="center", wrap_text="True")
+ws.cell(row,col+3).alignment = Alignment(vertical="center", horizontal="center", wrap_text="True")
+
+share_array_A = []
+year_array_A = []
+array_corrected_y_A = []
+array_corrected_x_A = []
+share_array_B = []
+year_array_B = []
+array_corrected_y_B = []
+array_corrected_x_B = []
 API_URL = "https://www.alphavantage.co/query"
 
-data = {
+data_A = {
 	"function": "SMA",
 	"time_period": 10,
 	"series_type": "open",
-	"symbol": stock_symbol,
+	"symbol": stock_symbol_A,
+	"interval": "monthly",
+	"apikey": "ksjendcksnfk",
+}
+
+data_B = {
+	"function": "SMA",
+	"time_period": 10,
+	"series_type": "open",
+	"symbol": stock_symbol_B,
 	"interval": "monthly",
 	"apikey": "ksjendcksnfk",
 }
@@ -50,46 +93,77 @@ proxies = {
 	"https" : "http://proxy-us.intel.com:911"
 }
 try:
-    response = requests.get(API_URL, params=data,proxies=proxies)
+    response_A = requests.get(API_URL, params=data_A,proxies=proxies)
+    response_B = requests.get(API_URL, params=data_B,proxies=proxies)
 except Exception:
-    response = requests.get(API_URL, params=data)
-arr = response.text.split("{")
+    response_A = requests.get(API_URL, params=data_A)
+    response_B = requests.get(API_URL, params=data_B)
+arr_A = response_A.text.split("{")
+arr_B = response_B.text.split("{")
 
-js = json.loads(response.text)
-d = js["Technical Analysis: SMA"]
-row = 1
+js_A = json.loads(response_A.text)
+js_B = json.loads(response_B.text)
+d_A = js_A["Technical Analysis: SMA"]
+d_B = js_B["Technical Analysis: SMA"]
+row = 2
 col = 1
 
-for s in d:
+for s in d_A:
 	if(row>1):
 	    stringer_a = str(s)
-	    stringer_b = str(d[s]["SMA"])
-	    year_array.append(str(stringer_a))
-	    share_array.append(float(stringer_b))
+	    stringer_b = str(d_A[s]["SMA"])
+	    year_array_A.append(str(stringer_a))
+	    share_array_A.append(float(stringer_b))
 	    print(stringer_a+"\t"+stringer_b)
-	    ws.cell(row-1,col,stringer_a)
-	    ws.cell(row-1,col+1,stringer_b)
+	    ws.cell(row,col,stringer_a).alignment = Alignment(vertical="top", horizontal="center", wrap_text="True")
+	    ws.cell(row,col+1,stringer_b).alignment = Alignment(vertical="top", horizontal="center", wrap_text="True")
+	row+=1
+row = 2
+col = 3
+
+for s in d_B:
+	if(row>1):
+	    stringer_a = str(s)
+	    stringer_b = str(d_B[s]["SMA"])
+	    year_array_B.append(str(stringer_a))
+	    share_array_B.append(float(stringer_b))
+	    print(stringer_a+"\t"+stringer_b)
+	    ws.cell(row,col,stringer_a).alignment = Alignment(vertical="top", horizontal="center", wrap_text="True")
+	    ws.cell(row,col+1,stringer_b).alignment = Alignment(vertical="top", horizontal="center", wrap_text="True")
 	row+=1
 wb.save("results.xlsx")
-year_array =  matplotlib.dates.datestr2num(year_array)
+year_array_A =  matplotlib.dates.datestr2num(year_array_A)
+year_array_B =  matplotlib.dates.datestr2num(year_array_B)
 formatter = dates.DateFormatter('%Y-%m-%d')
-for x in range(len(year_array)-1,-1,-1):
-	array_corrected_x.append(year_array[x])
-for x in range(len(share_array)-1,-1,-1):
-	array_corrected_y.append(share_array[x])
+for x in range(len(year_array_A)-1,-1,-1):
+	array_corrected_x_A.append(year_array_A[x])
+for x in range(len(share_array_A)-1,-1,-1):
+	array_corrected_y_A.append(share_array_A[x])
 
-array_corrected_x = getLeTicks(array_corrected_x)
-array_corrected_y = getLeTicks(array_corrected_y)
+for x in range(len(year_array_B)-1,-1,-1):
+	array_corrected_x_B.append(year_array_B[x])
+for x in range(len(share_array_B)-1,-1,-1):
+	array_corrected_y_B.append(share_array_B[x])
+
+array_corrected_x_A = getLeTicks(array_corrected_x_A)
+array_corrected_y_A = getLeTicks(array_corrected_y_A)
+array_corrected_x_B = getLeTicks(array_corrected_x_B)
+array_corrected_y_B = getLeTicks(array_corrected_y_B)
 plt.figure().patch.set_facecolor('xkcd:mint green')
-plt.plot(array_corrected_x,array_corrected_y, color='darkorange',marker='o')
+plt.plot(array_corrected_x_A,array_corrected_y_A, color='darkorange',marker='o')
+plt.plot(array_corrected_x_B,array_corrected_y_B, color='blue',marker='o')
+
 plt.xlabel("Dates")
 plt.ylabel("Share Value")
-plt.title("Share Value Trends for: "+stock_symbol)
+plt.title("Share Value Trends for: "+stock_symbol_A+" vs "+stock_symbol_B)
 plt.grid()
-the_ticks = array_corrected_x
-plt.xticks(the_ticks)
+the_ticks_A = array_corrected_x_A
+the_ticks_B = array_corrected_x_B
+plt.xticks(the_ticks_A)
 plt.xticks(rotation=70)
 ax = plt.gcf().axes[0] 
+
+plt.legend((stock_symbol_A,stock_symbol_B))
 ax.xaxis.set_major_formatter(formatter)
 
 plt.tight_layout()
